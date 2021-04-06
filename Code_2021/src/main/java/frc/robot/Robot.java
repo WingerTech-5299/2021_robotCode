6dba4fb43b9d58373fd46c8cb2821951f39158ce
Bryce Engberg <bryceengberg@gmail.com>
2021-04-06 10:13:27 -0500

    fad

--- Code_2021/src/main/java/frc/robot/Robot.java
+++ Code_2021/src/main/java/frc/robot/Robot.java
@@ -44,76 +44,6 @@ public class Robot extends TimedRobot {
   
   Boolean targetTest;
 
-  public void zeroEncoder(){
-
-    rightControllerB.setSelectedSensorPosition(0);
-    leftControllerB.setSelectedSensorPosition(0);
-
-  }
-
-  public void autoFind(){
-
-    while (tv == 0){
-      leftControllerB.setSelectedSensorPosition(0);
-      rightControllerB.setSelectedSensorPosition(0);
-
-      rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
-      leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
-
-      while ((rightControllerB.getSelectedSensorPosition() > -7000 && leftControllerB.getSelectedSensorPosition() > -7000) || (table.getEntry("tv").getDouble(0) == 0)){
-        tv = table.getEntry("tv").getDouble(0);
-        rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
-        leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
-
-        SmartDashboard.putNumber("EncoderR", rightEncoderPosition);
-        SmartDashboard.putNumber("EncoderL", leftEncoderPosition);
-
-      
-        drive.driveCartesian(0, 0, -0.5);
-        }
-
-      while ((rightControllerB.getSelectedSensorPosition() < 7000 && leftControllerB.getSelectedSensorPosition() < 7000) || (table.getEntry("tv").getDouble(0) == 0)){
-          tv = table.getEntry("tv").getDouble(0);
-          rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
-          leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
-  
-          SmartDashboard.putNumber("EncoderR", rightEncoderPosition);
-          SmartDashboard.putNumber("EncoderL", leftEncoderPosition);
-  
-        
-          drive.driveCartesian(0, 0, 0.5);
-          }
-
-      drive.driveCartesian(0, 0, 0);
-
-    }
-
-  }
-
-  public void autoPickUp(Double txFind, Double tyFind){
-
-    while (Math.abs(tx) > 0.2){
-
-      if (tx > 0){
-        drive.driveCartesian(0, 0, 0.5);
-      } else {
-        drive.driveCartesian(0, 0, 0.5);
-      }
-    }
-
-    Double balldistance = 17.75/Math.tan(ty);
-    Double unitsToBall = (balldistance/(Math.PI * 6)) * 4096;
-
-    zeroEncoder();
-
-    while (rightControllerB.getSelectedSensorPosition() < unitsToBall && leftControllerB.getSelectedSensorPosition() < 0){
-
-      drive.driveCartesian(0, 0.5, 0);
-
-    }
-
-  }
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -273,5 +203,77 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {  }
+
+  public void autoFind(){
+
+    while (tv == 0){
+      leftControllerB.setSelectedSensorPosition(0);
+      rightControllerB.setSelectedSensorPosition(0);
+
+      rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
+      leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
+
+      while ((rightControllerB.getSelectedSensorPosition() > -7000 && leftControllerB.getSelectedSensorPosition() > -7000) || table.getEntry("tv").getDouble(0) == 0){
+        tv = table.getEntry("tv").getDouble(0);
+        rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
+        leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
+
+        SmartDashboard.putNumber("EncoderR", rightEncoderPosition);
+        SmartDashboard.putNumber("EncoderL", leftEncoderPosition);
+
+      
+        drive.driveCartesian(0, 0, -0.5);
+        }
+
+      while ((rightControllerB.getSelectedSensorPosition() < 7000 && leftControllerB.getSelectedSensorPosition() < 7000) || (table.getEntry("tv").getDouble(0) == 0)){
+          tv = table.getEntry("tv").getDouble(0);
+          rightEncoderPosition = rightControllerB.getSelectedSensorPosition();
+          leftEncoderPosition = leftControllerB.getSelectedSensorPosition();
+  
+          SmartDashboard.putNumber("EncoderR", rightEncoderPosition);
+          SmartDashboard.putNumber("EncoderL", leftEncoderPosition);
+  
+        
+          drive.driveCartesian(0, 0, 0.5);
+          }
+
+      drive.driveCartesian(0, 0, 0);
+
+    }
+
+  }
+
+  public void zeroEncoder(){
+
+    rightControllerB.setSelectedSensorPosition(0);
+    leftControllerB.setSelectedSensorPosition(0);
+
+  }
+
+  
+
+  public void autoPickUp(Double txFind, Double tyFind){
+
+    while (Math.abs(tx) > 0.2){
+
+      if (tx > 0){
+        drive.driveCartesian(0, 0, 0.5);
+      } else {
+        drive.driveCartesian(0, 0, 0.5);
+      }
+    }
+
+    Double balldistance = 17.75/Math.tan(ty);
+    Double unitsToBall = (balldistance/(Math.PI * 6)) * 4096;
+
+    zeroEncoder();
+
+    while (rightControllerB.getSelectedSensorPosition() < unitsToBall && leftControllerB.getSelectedSensorPosition() < 0){
+
+      drive.driveCartesian(0, 0.5, 0);
+
+    }
+
+  }
 
 }
